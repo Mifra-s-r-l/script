4806f0cfd13e3813046ea90a3d275a10a4278f0d
Danny De Novi <70696078+dannydenovi@users.noreply.github.com>
2022-08-10 10:06:40 +0200

    Update static_ip_addr_rhel_fs.sh

--- static_ip_addr_rhel_fs.sh
+++ static_ip_addr_rhel_fs.sh
@@ -2,9 +2,9 @@
 
 #$2 -> ipaddr/CIDR  (es.192.168.1.2/24)
 
-UUID=`nmcli connection show | tail -1 | awk '{print $2}'`
+UUID=`nmcli connection show | tail -1 | awk '{print $1}'`
 
-sudo nmcli connection modify $UUID IPv4.address $2
+sudo nmcli connection modify $UUID IPv4.address $1
 sudo nmcli connection modify $UUID IPv4.gateway 192.168.1.1
 sudo nmcli connection modify $UUID IPv4.dns 8.8.8.8
 sudo nmcli connection modify $UUID IPv4.method manual
